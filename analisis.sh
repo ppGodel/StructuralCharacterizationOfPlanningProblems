456452ac95f2545691599da31172f5a7c6b06855
jhernandez <jhernandez@ppgodel.mx>
2017-10-22 21:12:56 -0500

    Added script

--- analisis.sh
+++ analisis.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+ADIR=$(pwd)/CompetitionsResults/IPC*
+BBDIR=$(pwd)/Files
+tg=100
+to=100
 
-ADIR=$(pwd)
-for dp in $ADIR/CompetitionsResults/IPC*;
+if [ ! -z "$1" ]; then
+    ADIR=$1
+fi
+if [ ! -z "$2" ]; then
+    BBDIR=$2
+fi
+if [ ! -z "$3" ] && [ $4> 0 ]; then
+    tg=$4
+fi
+if [ ! -z "$4" ] && [ $5 > 0 ]; then
+    to=$5
+fi
+
+for dp in $ADIR;
 do
     if [[ -d $dp ]]; then
         bn=$(basename "$dp")
@@ -9,12 +25,16 @@ do
 	for dcp in $dp/domains/*; do
 	    if [[ -d $dcp ]]; then
 		nbc=$(basename $dcp)
-		#echo "Problema" $nbc
-		echo $dcp
-		echo $(find $dcp -iname '*.ppdl')
-		for f in $(find $dcp -iname '*.ppdl' ); do
-		    echo "instancia:" $f
-                    #fn=$(basename $f)    
+		echo "Dominio" $nbc
+		domf=$(find $dcp -type f -iname '*domain*')
+		domn=$(basename $domf)
+		#echo $(find $dcp -type f -iname '*.pddl')
+		filelist=$(find $dcp -type f -iname '*.pddl' | sort -n)
+		for f in $filelist; do
+		    fn=$(basename $f)
+		    if [ "$fn" != "$domn" ]; then
+			$BBDIR/blackbox -o $domf -f $f -x -M 9999 -solver -maxsec $tg graphplan -then -maxsec $to walksat -then -maxsec $to satz -then -maxsec $to compact > $fn.txt
+		    fi
 		done
 	    fi
 	done
