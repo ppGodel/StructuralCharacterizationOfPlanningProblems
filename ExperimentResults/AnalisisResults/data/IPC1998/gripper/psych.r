ecdf65d14704590aa153a200e6efadc5b700c2a7
jhernandez <jhernandez@ppgodel.mx>
2018-01-26 10:07:30 -0600

    added some metrics

--- ExperimentResults/AnalisisResults/data/IPC1998/gripper/psych.r
+++ ExperimentResults/AnalisisResults/data/IPC1998/gripper/psych.r
@@ -1,7 +1,15 @@
 library('psych')
+library('plyr')
 dataraw = read.file(f="strips-gripper-all.csv")
 #describe(dataraw)
-aggregate(cbind(iid,odd)~gn, dat1, mean)
+tst=aggregate(id~gn+type+time, dataraw, count)
+boxplot(tst$id~tst$time)
 
+#aggregate(ixd~gn+type+time, dataraw, sum)
+tst=aggregate(ixd~gn+type+time, dataraw, mean)
+boxplot(tst$ixd~tst$time)
 
-library('plyr')
+#aggregate(oxd~gn+type+time, dataraw, sum)
+tst=aggregate(oxd~gn+type+time, dataraw, max)
+boxplot(tst$oxd~tst$time )
+
