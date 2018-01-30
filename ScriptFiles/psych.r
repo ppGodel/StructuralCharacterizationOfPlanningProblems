efc483a32634e67e8554c7c025d332dc567dc9f1
ppgodel <tashiro86@gmail.com>
2018-01-29 22:54:23 -0600

    added changes

--- ScriptFiles/psych.r
+++ ScriptFiles/psych.r
@@ -1,19 +1,41 @@
 library('psych')
+
+typg<<-"eps"
+imprimirini= function(typ, name){
+    typg<<-typ
+    if(typg=="eps"){
+        postscript(paste(name,".eps", sep=""))
+    }
+    else
+    {
+        png(paste(name,".eps", sep=""))
+    }
+    
+}
+imprimirfin= function(){
+    if(typg=="eps"){
+        dev.off()
+    }
+    else
+    {
+        graphics.off()
+    }
+    
+}
+
 #library('plyr')
-fn="gripper"
-dataraw = read.file(f=paste("strips-",fn,"-all.csv", sep=''))
-#describe(dataraw)
-#tst=aggregate(id~gn+type+time, dataraw, count)
-#boxplot(tst$id~tst$time)
-
-#aggregate(ixd~gn+type+time, dataraw, sum)
-png(paste(fn,"inexclusiveMeanByLevel.png", sep=''))
-tst=aggregate(ixd~gn+type+time, dataraw, mean)
-boxplot(tst$ixd~tst$time)
-graphics.off()
-#aggregate(oxd~gn+type+time, dataraw, sum)
-
-png(paste(fn,"outexclusiveMAXByLevel.png", sep=''))
-tst=aggregate(oxd~gn+type+time, dataraw, max)
-boxplot(tst$oxd~tst$time )
-graphics.off()
+#fn="gripper"
+fns=c("gripper", "log","mprime","movie")
+for(fn in fns){
+    dataraw = read.file(f=paste("strips-",fn,"-all.csv", sep=''))
+    
+    imprimirini(typ="eps",name=paste(fn,"inexclusiveMeanByLevel",sep=""))
+    tst=aggregate(ixd~gn+type+time, dataraw, mean)
+    boxplot(tst$ixd~tst$time)
+    imprimirfin()
+    
+    imprimirini(typ="eps",name=paste(fn,"outexclusiveMAXByLevel",sep=""))
+    tst=aggregate(oxd~gn+type+time, dataraw, max)
+    boxplot(tst$oxd~tst$time )
+    imprimirfin()
+}
